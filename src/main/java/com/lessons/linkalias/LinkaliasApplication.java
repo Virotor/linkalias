3b090a6ddb44638cc963bd3c9f8a7b94cc0ac5a5
Kvolian <virotor.vik@gmail.com>
2024-05-15 11:39:51 +0300

    Initial commit

---
+++ src/main/java/com/lessons/linkalias/LinkaliasApplication.java
@@ -0,0 +1,13 @@
+package com.lessons.linkalias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkaliasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkaliasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lessons/linkalias/LinkaliasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lessons.linkalias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkaliasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
